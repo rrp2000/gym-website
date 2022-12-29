63c51db87589cb94b8e774281cb840ccf7ef51e8
rrp2000 <ranjanpatel12391@gmail.com>
2022-12-29 16:27:38 +0530

    mentors added

--- src/program/Program.jsx
+++ src/program/Program.jsx
@@ -1,38 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./program.css";
 const Program = () => {
+  let [show,setShow] = useState("mentors")
   return (
     <div id="programContainter">
       <h2>Expore what we provide...</h2>
+      <div className="buttonContainer">
+        <button onClick={()=>setShow("mentors")} style={{backgroundColor:show==="mentors"?"white":"black", color:show==="mentors"?"black":"white", border:"none"}}>Mentors</button>
+        <button onClick={()=>setShow("facilities")} style={{backgroundColor:show==="facilities"?"white":"black", color:show==="facilities"?"black":"white", border:"none"}}>Facilities</button>
+      </div>
       <div className="cardContainer">
+        
         <div className="card">
           <img
+            // style={{width:"304px",height:"300px"}}
             className="cardImage"
-            src="assets/bench-press-56a83ae23df78cf7729d2c33.jpg"
+            src={show==="mentors"?"assets/mentor1 (1).jpg":"assets/facilities1.jpg"}
             alt=" workout"
           />
           <div className="cardDetails">
-            Best of the best traing facilities.
+          {show==="mentors"?"Henry wills":"Best of the best traing facilities."}
           </div>
         </div>
         <div className="card">
           <img
+            // style={{width:"304px",height:"300px"}}
             className="cardImage"
-            src="assets/pexels-pixabay-416754.jpg"
+            src={show==="mentors"?"assets/mentor2 (1).jpg":"assets/facilities3.jpg"}
             alt=" workout"
           />
           <div className="cardDetails">
-            Best of the best trainers.
+            {show==="mentors"?"Rosan Patel":"Best of the best trainers"}
           </div>
         </div>
         <div className="card">
           <img
+            // style={{width:"304px",height:"300px"}}
             className="cardImage"
-            src="assets/mark-bertulfo-XKimW0pke6w-unsplash.jpg"
+            src={show==="mentors"?"assets/mentor3 (1).jpg":"assets/facilities2.jpg"}
             alt=" workout"
           />
           <div className="cardDetails">
-            Best of the best equipments.
+          {show==="mentors"?"Julia Mires":"Best of the best equipments."}
           </div>
         </div>
       </div>
